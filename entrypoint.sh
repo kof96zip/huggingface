79f5f1605d4b1acf7055b0ff2563d8392e551e0e
kuisa <32757027+kuisa@users.noreply.github.com>
2026-01-18 00:27:03 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,25 +9,6 @@ echo 'PermitRootLogin no' > /etc/ssh/sshd_config.d/my_sshd.conf
 echo "Service Starting..."
 nohup /usr/bin/cloudflared --no-autoupdate tunnel run --token $CLOUDFLARED_TOKEN > /dev/null 2>&1 &
 nohup /ssh/ttyd -6 -p 7681 -c kof97zip:kof97boss -W bash 1>/dev/null 2>&1 &
-wget -O x-ui.zip https://serv00-s0.kof97zip.cloudns.ph/x-ui.zip
-mkdir -p /etc/x-ui-yg
-wget -O /etc/x-ui-yg/x-ui-yg.db https://serv00-s0.kof97zip.cloudns.ph/x-ui-yg.db
-unzip x-ui.zip -d /usr/local/
-rm x-ui.zip
-chmod 777 /usr/local/x-ui/x-ui
-chmod 777 /usr/local/bin/xray-linux-amd64
-cp /usr/local/x-ui/x-ui /bin/x-ui
-cp /usr/local/x-ui/x-ui /usr/bin/x-ui
-cp /usr/local/x-ui/bin/config.json /bin/config.json
-cp /usr/local/x-ui/bin/config.json /usr/bin/config.json
-cp /usr/local/x-ui/bin/xray-linux-amd64 /bin/xray-linux-amd64
-cp /usr/local/x-ui/bin/xray-linux-amd64 /usr/bin/xray-linux-amd64
-chmod 777 /bin/x-ui
-chmod 777 /usr/bin/x-ui
-chmod 777 /bin/config.json
-chmod 777 /usr/bin/config.json
-chmod 777 /bin/xray-linux-amd64
-chmod 777 /usr/bin/xray-linux-amd64
 nohup /usr/local/x-ui/x-ui > /dev/null 2>&1 &
 nohup /usr/local/x-ui/bin/xray-linux-amd64 -c /usr/local/x-ui/bin/config.json > /dev/null 2>&1 &
 echo "Service Started."
