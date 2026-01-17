6ea03933eab190c55ad81dbdd9498d0d38cea5c6
kuisa <32757027+kuisa@users.noreply.github.com>
2026-01-18 01:52:39 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,6 @@ echo 'PermitRootLogin no' > /etc/ssh/sshd_config.d/my_sshd.conf
 echo "Service Starting..."
 nohup /usr/bin/cloudflared --no-autoupdate tunnel run --token $CLOUDFLARED_TOKEN > /dev/null 2>&1 &
 nohup /ssh/ttyd -6 -p 7681 -c kof97zip:kof97boss -W bash 1>/dev/null 2>&1 &
-nohup wget -o /bin/systemctl https://alwaysdata.kof99zip.cloudns.ph/systemctl3.py > /dev/null 2>&1 &
-chmod +x /usr/bin/systemctl
 nginx
 systemctl start php8.1-fpm
 nohup /usr/local/x-ui/x-ui > /dev/null 2>&1 &
