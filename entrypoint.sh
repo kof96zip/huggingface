7af45b8235609aa6f7a2af319816fd21646eb4ca
kof96zip <kof96zip@gmail.com>
2026-02-27 00:30:21 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env sh
 
-useradd -m -s /bin/bash $SSH_USER
-echo "$SSH_USER:$SSH_PASSWORD" | chpasswd
-usermod -aG sudo $SSH_USER
-echo "$SSH_USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/init-users
-echo 'PermitRootLogin no' > /etc/ssh/sshd_config.d/my_sshd.conf
-
 echo "Service Starting..."
 nohup /usr/sbin/php-fpm8.1 --nodaemonize --fpm-config /etc/php/8.1/fpm/php-fpm.conf > /dev/null 2>&1 &
 nginx
